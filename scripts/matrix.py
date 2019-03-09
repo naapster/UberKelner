0c46bb58a3458d8bbd7815b43d7b386a70ae20c9
Anteras2 <anteras4@gmail.com>
2019-03-09 20:24:49 +0100

    Dodanie komentarzy

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -3,10 +3,12 @@
 # comments to be added - Marcin halko
 
 class Matrix:
+    #konstruktor, ustawia ilosc szeregow i kolumn, mozna rowniez ustawic co ma byc podstawowym zapelnieniem macierzy, jezeli nie zmienione to zero
     def __init__(self, rows, columns, fill=0):
         self.matrix = [[fill for x in range(columns)] for y in range(rows)]
 
     def __repr__(self):
+        #funkcja repr pozwala na ladne drukowanie macierzy, mozna zrobic print(Matrx) i zobaczymy ladna macierz
         s = [[str(e) for e in row] for row in self.matrix]
         lens = [max(map(len, col)) for col in zip(*s)]
         fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
@@ -15,6 +17,7 @@ class Matrix:
         return represent
 
     def print_matrix(self):
+        #zbedne gdy mamy repr ale nadal dodalem, mozna wydrukowac macierz metoda, jak kto woli
         s = [[str(e) for e in row] for row in self.matrix]
         lens = [max(map(len, col)) for col in zip(*s)]
         fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
@@ -22,6 +25,7 @@ class Matrix:
         print('\n'.join(table))
 
     def insert_object(self, object_to_insert, row, column, debug=False):
+        #wklada obiekt na podane miejsce do macierzy
         try:
             self.matrix[row][column] = object_to_insert
 
@@ -31,6 +35,7 @@ class Matrix:
             print("ERROR: Index out of bounds. Inserting object is not possible.")
 
     def delete_object(self, row, column, debug=False):
+        #usuwa obiekt i zastepuje go 0, do poprawy
         try:
             deleted_object = self.matrix[row][column]
             self.matrix[row][column] = 0
@@ -41,6 +46,7 @@ class Matrix:
             print('ERROR: Index out of bounds. Deleting object is not possible.')
 
     def objects_to_list(self, wanted_object):
+        #zwraca liste obiektow i ich miejsc w macierzy, porownywany jest typ obiektu, nie jego wartosc
         list_of_wanted_objects = list()
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix)):
