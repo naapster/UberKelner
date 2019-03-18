08e2d6fea55ce7905f742ce8adab83fc0ba2798e
Adam Lewicki <adalew1@st.amu.edu.pl>
2019-03-18 18:10:45 +0100

    update

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -1,70 +1,79 @@
-
 # matrix object class
 import copy
 
 class Matrix:
-    #konstruktor, ustawia ilosc szeregow i kolumn, mozna rowniez ustawic co ma byc podstawowym zapelnieniem macierzy, jezeli nie zmienione to zero
+    # matrix init, set rows and columns, fill is optional - 0 by default
     def __init__(self, rows, columns, fill=0):
         self.fill = fill
         self.matrix = [[self.fill for x in range(columns)] for y in range(rows)]
 
-    def __repr__(self):
-        #funkcja repr pozwala na ladne drukowanie macierzy, mozna zrobic print(Matrx) i zobaczymy ladna macierz
-        s = [[str(e) for e in row] for row in self.matrix]
-        lens = [max(map(len, col)) for col in zip(*s)]
-        fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-        table = [fmt.format(*row) for row in s]
-        represent = '\n'.join(table)
-        return represent
-
+    # print matrix content
     def print_matrix(self):
-        #zbedne gdy mamy repr ale nadal dodalem, mozna wydrukowac macierz metoda, jak kto woli
-        s = [[str(e) for e in row] for row in self.matrix]
+        s = [[str(e).split(' ', 1)[0] for e in row] for row in self.matrix]
         lens = [max(map(len, col)) for col in zip(*s)]
         fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
         table = [fmt.format(*row) for row in s]
         print('\n'.join(table))
         print("------------------------------------")
 
-    def insert_object(self, object_to_insert, debug=False):
-        #wklada obiekt na podane miejsce do macierzy
-        try:
+    def __repr__(self):
+        self.print_matrix()
+
+    # put object on its own coordinates
+    def simple_insert(self, object_to_insert):
+        if self.is_empty(object_to_insert.x,object_to_insert.y):
             self.matrix[object_to_insert.x][object_to_insert.y] = object_to_insert
+            return True
+        else:
+            return False
 
-            if debug:
-                print('Added {0} to matrix[{1}][{2}]'.format(object_to_insert, object_to_insert.x, object_to_insert.y))
-        except IndexError:
-            print("ERROR: Index out of bounds. Inserting object is not possible.")
+    # put object on coordinates
+    def insert(self, object_to_insert, x, y):
+        if self.is_empty(x, y):
+            self.matrix[x][y] = object_to_insert
+            return True
+        else:
+            return False
 
-    def delete_object(self, row, column, debug=False):
-        #usuwa obiekt i zastepuje go 0, do poprawy
-        try:
-            deleted_object = self.matrix[row][column]
-            self.matrix[row][column] = self.fill
+    # remove object and set 0 instead - REPAIR
+    def delete_object(self, x, y):
+        if not self.is_empty(x, y):
+            self.matrix[x][y] = self.fill
+            return True
+        else:
+            return False
 
-            if debug:
-                print('Deleted {0} from matrix[{1}][{2}]'.format(deleted_object, row, column))
-        except IndexError:
-            print('ERROR: Index out of bounds. Deleting object is not possible.')
 
+    # move object form coordinates to new one
+    def move(self, x, y, new_x, new_y):
+        if not self.is_empty(x,y) and self.is_empty(new_x,new_y):
+            self.matrix[new_x][new_y] = self.matrix[x][y]
+            self.matrix[x][y] = self.fill
+            return True
+        else:
+            return False
+
+    # returns list of objects and coordinates - checking object class type, not content
     def objects_to_list(self, wanted_object):
-        #zwraca liste obiektow i ich miejsc w macierzy, porownywany jest typ obiektu, nie jego wartosc
         list_of_wanted_objects = list()
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix)):
                 if type(self.matrix[i][j]) is type(wanted_object):
                     list_of_wanted_objects.append((self.matrix[i][j], (i, j)))
-
         return list_of_wanted_objects
 
+    # check if coordinates are empty
+    def is_empty(self, x, y):
+        try:
+            return self.matrix[x][y] == self.fill
+        except IndexError:
+            return False
+
+    # ?
     def get_matrix(self):
         to_return = copy.deepcopy(self.matrix)
         return to_return
 
+    # ?
     def get_empty_space_type(self):
         return self.fill
-
-if __name__ == '__main__':
-    mat = Matrix(30, 30, fill='X')
-    print(mat)
-    print(mat.get_matrix())
--- scripts/waiter.py
+++ scripts/waiter.py
@@ -3,7 +3,6 @@
 from scripts.matrix import *
 from scripts.dinning_table import *
 from scripts.furnace import *
-from scripts.__init__ import *
 import pygame
 from pygame.locals import *
 
@@ -29,19 +28,19 @@ class Waiter(pygame.sprite.Sprite):
         init_graphics(self, self.x, self.y, "waiter")
 
         # add objects to restaurant - creates waiters, tables and furnaces basing on random positions in the matrix
-        # objects have coordinates like in matrix (0..N, 0..N)
-
+        # objects have coordinates like in matrix (0..N, 0..N):
+        # add waiter to restaurant
+        self.restaurant.insert('Waiter', self.x, self.y)
         counter = 1
-
+        # add tables
         for i in range(num_tables):
             self.dining_tables.append([matrix_fields[i + counter][0], matrix_fields[i + counter][1]])
-            self.restaurant.insert_object(Dinning_table(matrix_fields[i + counter][0], matrix_fields[i + counter][1]), debug=True)
-
+            self.restaurant.simple_insert(Dinning_table(matrix_fields[i + counter][0], matrix_fields[i + counter][1]))
         counter += num_tables
-
+        # add furnaces
         for i in range(num_furnaces):
             self.furnaces.append([matrix_fields[i + counter][0], matrix_fields[i + counter][1]])
-            self.restaurant.insert_object(Furnace(matrix_fields[i + counter][0], matrix_fields[i + counter][1]), debug=True)
+            self.restaurant.simple_insert(Furnace(matrix_fields[i + counter][0], matrix_fields[i + counter][1]))
 
     # movement procedure - change position on defined difference of coordinates
     def move(self, delta_x, delta_y):
@@ -49,8 +48,9 @@ class Waiter(pygame.sprite.Sprite):
         new_y = self.y + delta_y
         # if movement is within restaurant borders
         if new_x in range(0, N) and new_y in range(0, N):
-            # and the field is empty
-            if [new_x, new_y] not in self.dining_tables and [new_x, new_y] not in self.furnaces:
+            # if movement is allowed by matrix and the field is empty:
+            if self.restaurant.insert(self, new_x, new_y):
+                self.restaurant.delete_object(self.x, self.y)
                 # set new coordinates
                 self.x = new_x
                 self.y = new_y
@@ -80,15 +80,4 @@ class Waiter(pygame.sprite.Sprite):
             furnace[0].next_round()
 
         # show me status of simulation
-        #self.restaurant.print_matrix()
-
-    def example(self):
-        # example usage of matrix, for development purpose only
-        self.space = Matrix(N, N)
-        self.space.print_matrix()
-        self.space.insert_object('asdasd', 2, 4, debug=True)
-        self.space.insert_object(Matrix(2, 2, fill=5), 1, 1)
-        self.space.print_matrix()
-        print(self.space.objects_to_list('asdasd'))
-        self.space.delete_object(1, 1, debug=True)
-        self.space.print_matrix()
+        self.restaurant.print_matrix()
